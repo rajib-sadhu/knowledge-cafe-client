7679ec9f07a92ab4128e7e7337e9b7996a8bd8b4
rajib-sadhu <rahulrajib.sadhu@gmail.com>
2023-03-31 17:15:05 +0530

    change style in bookmarked

--- src/components/BookMark/BookMark.jsx
+++ src/components/BookMark/BookMark.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const BookMark = ({bookmark, spent}) => {
     
     return (
-        <div className='sticky top-0 pt-6'>
+        <div className='sticky top-0 pt-6 pb-2 md:px-0 px-2'>
             <div className='bg-slate-200 p-4 rounded-md text-center border border-violet-800'>
             <h1 className='font-semibold text-violet-800'>Spent time on read: {spent.reduce((a,c)=>a+c,0)} mins</h1>
             </div>
